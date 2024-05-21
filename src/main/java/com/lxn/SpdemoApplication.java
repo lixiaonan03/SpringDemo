a42f96150df67761b85c9e03a81fcb1b75536984
lixiaonan <472379153@qq.com>
2024-05-21 15:29:28 +0800

    加入ssl 证书双向校验的功能

--- src/main/java/com/lxn/SpdemoApplication.java
+++ src/main/java/com/lxn/SpdemoApplication.java
@@ -1,16 +1,50 @@
 package com.lxn;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-public class SpdemoApplication {
+public class SpdemoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpdemoApplication.class, args);
 	}
 
+	@Bean
+	public Connector connector(){
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(80);
+		connector.setSecure(false);
+		//监听到http的端口号后转向到的https的端口号,也就是项目配置的port
+		connector.setRedirectPort(443);
+		return connector;
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+		TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint constraint = new SecurityConstraint();
+				constraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				constraint.addCollection(collection);
+				context.addConstraint(constraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(connector);
+		return tomcat;
+	}
 }
--- src/main/java/com/lxn/controller/LiController.java
+++ src/main/java/com/lxn/controller/LiController.java
@@ -5,7 +5,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import springfox.documentation.service.Parameter;
+
+import org.apache.coyote.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 import java.util.Collections;
@@ -20,6 +27,8 @@ import java.util.Map;
 @Api(tags = "BookController", description = "BookController | 通过书来测试swagger")
 public class LiController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LiController.class);
+
     Map<String,Book> books = Collections.synchronizedMap(new HashMap<String, Book>());
 
     @ApiOperation(value="创建图书", notes="创建图书")
@@ -29,9 +38,28 @@ public class LiController {
         books.put(book.getId(), book);
         return "success";
     }
+
+    @RequestMapping(value="/file", method=RequestMethod.POST)
+    public String putFile(MultipartFile file) {
+        return "success";
+    }
+
+    @RequestMapping(value = "/post_user", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String setConstructKeyPoints( @RequestBody Parameter parameter) {
+        return "success";
+    }
+
+
+
     @ApiOperation(value = "hello2", notes = "hello2的")
     @RequestMapping(value = "/hello2", method = RequestMethod.GET)
     public String say1(@ApiParam(name="id",value="用户id",required=true)String a, Integer b, BigDecimal c) {
+        try {
+            logger.info("接受到了"+Thread.currentThread().getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        logger.info("准备回应的==="+Thread.currentThread().getName());
         return "---蝶衣-----";
     }
 
